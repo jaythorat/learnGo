a44e39af8cf256558f37fcc2b48a657a5902b0a1
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-16 08:37:03 +0530

    Function and error handling

--- main.go
+++ main.go
@@ -8,6 +8,17 @@ type User struct {
 }
 
 
+func add(a,b int) int {
+	return a+b
+}
+
+func divide (a,b int) (int,error){
+	if b == 0 {
+		return 0, fmt.Errorf("divide by zero")
+	}
+	return a/b,nil
+}
+
 
 func (u User) Greet() string {
 	return "Hello " + u.Name
@@ -18,5 +29,15 @@ func main() {
 	u := User{"Jay",25}
 	a := u.Greet()
 
-    fmt.Println("Hello, Go!",a)
+	add := add(10,10)
+
+	d,error := divide(10,0)
+	if error != nil {
+		fmt.Println("Errror :",error)
+	}
+
+	fmt.Println(d)
+
+
+    fmt.Println("Hello, Go!",a,add)
 }
