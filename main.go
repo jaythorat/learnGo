1f6dc5cad2881d79a1f515e4f4d3eea77834aa5b
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-16 08:58:40 +0530

    Goroutine + chanell basic

--- main.go
+++ main.go
@@ -1,43 +1,27 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"time")
 
-type User struct {
-	Name string
-	Age int
+
+func tp(ch chan string){
+	time.Sleep(1 * time.Second)	
+	fmt.Println("HI",<- ch)
+	ch <- "Gora"
 }
 
 
-func add(a,b int) int {
-	return a+b
-}
-
-func divide (a,b int) (int,error){
-	if b == 0 {
-		return 0, fmt.Errorf("divide by zero")
-	}
-	return a/b,nil
-}
-
-
-func (u User) Greet() string {
-	return "Hello " + u.Name
-}
 
 
 func main() {
-	u := User{"Jay",25}
-	a := u.Greet()
+	ch := make(chan string)
+	// go sayHello(ch)
+	// msg := <- ch
 
-	add := add(10,10)
-
-	d,error := divide(10,0)
-	if error != nil {
-		fmt.Println("Errror :",error)
-	}
-
-	fmt.Println(d)
+	go tp(ch)
+	ch <- "Jayyyyyyy"
+	fmt.Println(" Jay")
+	fmt.Print(<-ch)
 
 
-    fmt.Println("Hello, Go!",a,add)
 }
