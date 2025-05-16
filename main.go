fb205c45c67b5d9db99d78a441bf2d43972726b8
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-16 12:24:21 +0530

    barebone basic http server

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 )
 
-func main() {
+type HelloResponse struct {
+	Message string `json:"message11"`
+	Name string 
+}
 
-	fmt.Println(Hi("Jay"))
+func Handler(w http.ResponseWriter, r *http.Request){
+	resp := HelloResponse{Message: "HI JAY",Name: "jay"}
+	json.NewEncoder(w).Encode(resp)
+}
+
+
+
+func main() {
+	http.HandleFunc("/hello",Handler)
+	http.ListenAndServe(":8080",nil)
 
 }
