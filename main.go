8b2614c385c991f6a51594cf9ee60949a94b9a11
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-16 11:40:39 +0530

    array slice , map

--- main.go
+++ main.go
@@ -4,24 +4,32 @@ import ("fmt")
 
 
 
-
-
-
-
 func main() {
 	
-	var age int = 19
-	switch day:= "monday";day { // one line init and pass
-	case "monday":
-	fmt.Println("mon")
-	case "2":
-		fmt.Println("Jay")
-	default:
-		fmt.Println("xxx")
+	var arr [4]int = [4]int{1,2,3,4}
+	arr2 := [4]int {5,6,7,8}
 
+	fmt.Println(arr,arr2)
+
+	nums := []int{1,2,3,4,4}
+	fmt.Println(nums)
+	nums2 := []int{99,99,99}
+	nums = append(nums, nums2...)
+	fmt.Println(nums)
+
+	user := map[any]any{1:25, // [string]any  any and interface{} both same thing
+		"name":"JAY",
 	}
 
-	fmt.Println(age)
+	for k,v := range user {
+		fmt.Println(k,v,"|")
+	}
+
+	fmt.Println(user,user["age"])
+
+
+
+	// fmt.Println(arr,arr[0],arr2,arr2[len(arr2)])
 
 
 }
