d2cb7253f99ec9fad5fdfdbcf9db8b1656a5c326
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-16 11:41:43 +0530

    array slice , map

--- main.go
+++ main.go
@@ -6,26 +6,9 @@ import ("fmt")
 
 func main() {
 	
-	var arr [4]int = [4]int{1,2,3,4}
-	arr2 := [4]int {5,6,7,8}
+	a := 1
 
-	fmt.Println(arr,arr2)
-
-	nums := []int{1,2,3,4,4}
-	fmt.Println(nums)
-	nums2 := []int{99,99,99}
-	nums = append(nums, nums2...)
-	fmt.Println(nums)
-
-	user := map[any]any{1:25, // [string]any  any and interface{} both same thing
-		"name":"JAY",
-	}
-
-	for k,v := range user {
-		fmt.Println(k,v,"|")
-	}
-
-	fmt.Println(user,user["age"])
+	fmt.Println(a)
 
 
 
